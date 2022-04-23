75c32859e54a4571ec51650791aab3fbbff9ddc3
Aman Kapoor <99702502+MrAmanKapoor@users.noreply.github.com>
2022-04-23 14:25:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function () {
+    $("body").on("contextmenu", function (e) {
+        return false;
+    });
+
+});
+$(document).bind('keydown', function (e) {
+    if (e.ctrlKey && (e.which == 83)) {
+        e.preventDefault();
+
+        return false;
+    }
+    else if (e.ctrlKey && (e.which == 85)) {
+        e.preventDefault();
+
+        return false;
+
+    }
+
+});
