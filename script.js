262bdc70e94469143baefff690c0c295dd0d2874
Aman Kapoor <amankapoor140@gmail.com>
2022-07-17 23:55:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,26 @@ $(document).bind('keydown', function (e) {
     }
 
 });
+
+
+/*
+$(document).ready(function () {
+    $("body").on("contextmenu", function (e) {
+        return false;
+    });
+
+});
+$(document).bind('keydown', function (e) {
+    if (e.ctrlKey && (e.which == 83)) {
+        e.preventDefault();
+
+        return false;
+    }
+    else if (e.ctrlKey && (e.which == 85)) {
+        e.preventDefault();
+
+        return false;
+
+    }
+
+});*/
